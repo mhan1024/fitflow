17258317a24617cb683482c142fe45d32bec4c37
Michelle Han <mhan10242019@gmail.com>
2025-10-20 16:00:52 -0400

    restarted proj

--- dashboard_backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import signup_view, login_view, current_user, logout_view
-
-urlpatterns = [
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
-    path('me/', current_user, name='current'),
-    path('logout/', logout_view, name='logout'),
-]
---
+++ fitflow_frontend/src/pages/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home-page">
+        <h1>HOME</h1>
+        
+    </div>
+</template>
+
